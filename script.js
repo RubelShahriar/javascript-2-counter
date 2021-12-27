dcf87ef1f9bf69dd26fd713c2e93cc99af6040d5
RubelShahriar <mr.rubelshahriar@gmail.com>
2021-12-28 00:16:15 +0600

    counter part two

--- script.js
+++ script.js
@@ -14,15 +14,6 @@ btns.forEach(function(btn){
         else{
             count = 0
         }
-        if(count < 0){
-            value.style.color = 'red'
-        }
-        if(count > 0){
-            value.style.color = 'springgreen'
-        }
-        if(count === 0){
-            value.style.color = 'black'
-        }
         value.textContent = count
     })
 })
